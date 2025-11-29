f595e7af312b1f88a08979b89465a4fb04e006cb
= <sagarsoni32896@gmail.com>
2025-11-29 15:25:25 +0530

    2nd

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,12 @@ import ChildTwo from "./ChildComponent2";
 const App = () => {
   let [selectedOption,setselectedOption] = useState("");
   return (
-    <div style={{height:"40%",width:"100%",backgroundColor:"green"}}>
+    <div style={{height:"40%",width:"100%",backgroundColor:"green"}} className="parent">
         {/* Do not remove the main div */}
         <h1>Parent Component</h1>
         <ChildOne setselectedOption={setselectedOption}/>
         <ChildTwo setselectedOption={setselectedOption}/>
-        <p>Selected Option : {selectedOption != ""?selectedOption:selectedOption}</p>
+        <p>Selected Option: {selectedOption != ""?selectedOption:selectedOption}</p>
 
     </div>
   )
